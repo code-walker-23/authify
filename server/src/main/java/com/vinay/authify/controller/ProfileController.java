de2de4e25a795046509fbfdb9a8c16e71b66edee
code-walker-23 <chhabravinay549@gmail.com>
2025-06-03 01:40:02 +0530

    Added ProfileController

---
+++ server/src/main/java/com/vinay/authify/controller/ProfileController.java
@@ -0,0 +1,24 @@
+package com.vinay.authify.controller;
+
+import com.vinay.authify.dto.ProfileRequest;
+import com.vinay.authify.dto.ProfileResponse;
+import com.vinay.authify.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1.0")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProfileResponse register(@RequestBody ProfileRequest request){
+        ProfileResponse response = profileService.createProfile(request);
+        // TODO: Trigger an email with welcome
+        return response;
+    }
+}
