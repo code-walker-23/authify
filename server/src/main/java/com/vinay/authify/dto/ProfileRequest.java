3282cc25e8ab75935a82e02e95cabe2002fdf1a0
code-walker-23 <chhabravinay549@gmail.com>
2025-06-03 00:49:20 +0530

    added lombok features

--- server/src/main/java/com/vinay/authify/dto/ProfileRequest.java
+++ server/src/main/java/com/vinay/authify/dto/ProfileRequest.java
@@ -1,5 +1,12 @@
 package com.vinay.authify.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProfileRequest {
     private String name;
     private String email;
