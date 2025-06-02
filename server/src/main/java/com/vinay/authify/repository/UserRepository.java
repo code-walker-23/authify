0ac9e99a741fcbdaadb80f76cb1beb571d03364a
code-walker-23 <chhabravinay549@gmail.com>
2025-06-02 23:28:33 +0530

    Added UserRepository

---
+++ server/src/main/java/com/vinay/authify/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.vinay.authify.repository;
+
+import com.vinay.authify.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
