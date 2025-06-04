e2e4b553ee078a78489380dc45c0704d22351c48
code-walker-23 <chhabravinay549@gmail.com>
2025-06-04 14:21:54 +0530

    Added Security Config

--- server/src/main/java/com/vinay/authify/config/SecurityConfig.java
+++ server/src/main/java/com/vinay/authify/config/SecurityConfig.java
@@ -1,23 +1,55 @@
 package com.vinay.authify.config;
 
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api").permitAll()  // Allow registration
-                .anyRequest().authenticated()           // Secure other endpoints
-                .and()
-                .httpBasic();                           // Enables basic auth
-
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http.cors(Customizer.withDefaults())
+               .csrf(AbstractHttpConfigurer::disable)
+               .authorizeHttpRequests(auth -> auth.requestMatchers("/login","/register","/logout","/send-rest-otp","/send-reset-password").permitAll().anyRequest().authenticated())
+               .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .logout(AbstractHttpConfigurer::disable);
+               return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        return new CorsFilter(corsConfigurationSource());
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://localhost:5173"));
+        config.setAllowedMethods(List.of("GET","PUT","DELETE","UPDATE","PATCH","OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",config);
+
+        return source;
+    }
+
 }
