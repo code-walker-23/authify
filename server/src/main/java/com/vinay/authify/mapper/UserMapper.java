ba9376d9af874d0dc9e8d849312cca31cea8f104
code-walker-23 <chhabravinay549@gmail.com>
2025-06-05 20:10:50 +0530

    Added PasswordEncoder for storing encoded password in DB

--- server/src/main/java/com/vinay/authify/mapper/UserMapper.java
+++ server/src/main/java/com/vinay/authify/mapper/UserMapper.java
@@ -3,19 +3,23 @@ package com.vinay.authify.mapper;
 import com.vinay.authify.dto.ProfileRequest;
 import com.vinay.authify.dto.ProfileResponse;
 import com.vinay.authify.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class UserMapper {
+    private final PasswordEncoder passwordEncoder;
 
     public UserEntity toEntity(ProfileRequest request) {
         return UserEntity.builder()
                 .email(request.getEmail())
                 .name(request.getName())
                 .userId(UUID.randomUUID().toString())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .isAccountVerified(false)
                 .verifyOtp(null)
                 .verifyOtpExpiredAt(0L)
