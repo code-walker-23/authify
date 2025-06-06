c67cc642a29054978586570620ac6bba5d3e0659
code-walker-23 <chhabravinay549@gmail.com>
2025-06-06 21:18:00 +0530

    Added Send Rest Otp Endpoint

--- server/src/main/java/com/vinay/authify/controller/AuthController.java
+++ server/src/main/java/com/vinay/authify/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.vinay.authify.controller;
 import com.vinay.authify.dto.AuthRequest;
 import com.vinay.authify.dto.AuthResponse;
 import com.vinay.authify.service.AppUserDetailsService;
+import com.vinay.authify.service.ProfileService;
 import com.vinay.authify.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -15,10 +16,8 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -32,6 +31,7 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final AppUserDetailsService appUserDetailsService;
     private final JwtUtil jwtUtil;
+    private final ProfileService profileService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest request){
@@ -71,4 +71,12 @@ public class AuthController {
         return ResponseEntity.ok(email != null);
     }
 
+    @PostMapping("/send-reset-otp")
+    public void sendResetOtp(@RequestParam String email){
+        try{
+            profileService.sendResetOtp(email);
+        }catch(Exception ex){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,ex.getMessage());
+        }
+    }
 }
