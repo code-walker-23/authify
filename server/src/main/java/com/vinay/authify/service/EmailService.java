7bd43c6f1595b02f19acc99ea1f64720cf799b2c
code-walker-23 <chhabravinay549@gmail.com>
2025-06-06 21:21:30 +0530

    Added Send Rest Otp Method

--- server/src/main/java/com/vinay/authify/service/EmailService.java
+++ server/src/main/java/com/vinay/authify/service/EmailService.java
@@ -22,4 +22,25 @@ public class EmailService {
         message.setText("Hello " + name + ",\n\nThanks for registering with us!\n\nRegards,\nAuthify Team");
         javaMailSender.send(message);
     }
+
+    public void sendResetOtp(String toEmail, String otp){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject("Your Password Reset OTP - Authify");
+
+        String body = String.format(
+                "Dear user,\n\n" +
+                        "We received a request to reset your password.\n\n" +
+                        "Your One-Time Password (OTP) is: %s\n\n" +
+                        "This OTP is valid for 15 minutes. Please do not share it with anyone.\n\n" +
+                        "If you did not request a password reset, please ignore this email or contact support immediately.\n\n" +
+                        "Best regards,\n" +
+                        "Authify Security Team",
+                otp
+        );
+
+        message.setText(body);
+        javaMailSender.send(message);
+    }
 }
