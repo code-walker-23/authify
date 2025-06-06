451f98e3072efead49bd9d15c0fc4c9510e5a7f7
code-walker-23 <chhabravinay549@gmail.com>
2025-06-06 18:31:34 +0530

    Added Profile Endpoint

--- server/src/main/java/com/vinay/authify/controller/ProfileController.java
+++ server/src/main/java/com/vinay/authify/controller/ProfileController.java
@@ -6,6 +6,7 @@ import com.vinay.authify.service.ProfileService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,5 +22,10 @@ public class ProfileController {
         ProfileResponse response = profileService.createProfile(request);
         // TODO: Trigger an email with welcome
         return response;
+    }
+
+    @GetMapping("/profile")
+    public ProfileResponse getProfile(@CurrentSecurityContext(expression = "authentication?.name") String email){
+        return profileService.getProfile(email);
     }
 }
