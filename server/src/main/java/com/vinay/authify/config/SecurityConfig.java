709234a2deaba8145b77c341cdf42222e8510037
code-walker-23 <chhabravinay549@gmail.com>
2025-06-06 15:17:47 +0530

    Registered Jwt Filter

--- server/src/main/java/com/vinay/authify/config/SecurityConfig.java
+++ server/src/main/java/com/vinay/authify/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.vinay.authify.config;
 
+import com.vinay.authify.filter.JwtRequestFilter;
 import com.vinay.authify.service.AppUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,6 +29,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final AppUserDetailsService appUserDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,8 +37,9 @@ public class SecurityConfig {
                .csrf(AbstractHttpConfigurer::disable)
                .authorizeHttpRequests(auth -> auth.requestMatchers("/login","/register","/logout","/send-rest-otp","/send-reset-password").permitAll().anyRequest().authenticated())
                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-               .logout(AbstractHttpConfigurer::disable);
-               return http.build();
+               .logout(AbstractHttpConfigurer::disable)
+               .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+       return http.build();
     }
 
     @Bean
@@ -52,7 +56,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("https://localhost:5173"));
         config.setAllowedMethods(List.of("GET","PUT","DELETE","UPDATE","PATCH","OPTIONS"));
-        config.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -69,5 +73,6 @@ public class SecurityConfig {
 
         return new ProviderManager(authenticationProvider);
     }
+
 
 }
