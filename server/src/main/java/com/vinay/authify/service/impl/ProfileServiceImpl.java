19fd55a97b95201b4d501cd4c9a2c9a6c07c9a8b
code-walker-23 <chhabravinay549@gmail.com>
2025-06-06 21:22:34 +0530

    Added Send Rest Otp Service

--- server/src/main/java/com/vinay/authify/service/impl/ProfileServiceImpl.java
+++ server/src/main/java/com/vinay/authify/service/impl/ProfileServiceImpl.java
@@ -6,6 +6,7 @@ import com.vinay.authify.dto.ProfileResponse;
 import com.vinay.authify.entity.UserEntity;
 import com.vinay.authify.mapper.UserMapper;
 import com.vinay.authify.repository.UserRepository;
+import com.vinay.authify.service.EmailService;
 import com.vinay.authify.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
@@ -14,12 +15,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 @RequiredArgsConstructor
 public class ProfileServiceImpl implements ProfileService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final EmailService emailService;
 
     @Override
     public ProfileResponse createProfile(ProfileRequest request) {
@@ -38,5 +42,31 @@ public class ProfileServiceImpl implements ProfileService {
         UserEntity user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found "+ email));
         return userMapper.toResponse(user);
+    }
+
+    @Override
+    public void sendResetOtp(String email) {
+        UserEntity existingUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with " + email));
+
+        // Generating 6 digit otp
+        String otp = String.valueOf(ThreadLocalRandom.current().nextInt(100000,1000000));
+
+        // Expiry time 15 min in miliseconds
+        long expiryTime = System.currentTimeMillis() + (15 * 60 * 1000);
+
+        // update the user profile
+        existingUser.setResetOtp(otp);
+        existingUser.setResetOtpExpiredAt(expiryTime);
+
+        // save into the database
+        userRepository.save(existingUser);
+
+        try {
+            emailService.sendResetOtp(existingUser.getEmail(), otp);
+        }catch(Exception ex){
+            throw new RuntimeException("Unable to send email");
+        }
+
     }
 }
