783ebc2086ecb04b69b18c5e6828cf51a2391c93
code-walker-23 <chhabravinay549@gmail.com>
2025-06-06 21:22:18 +0530

    Added Send Rest Otp Service

--- server/src/main/java/com/vinay/authify/service/ProfileService.java
+++ server/src/main/java/com/vinay/authify/service/ProfileService.java
@@ -6,4 +6,5 @@ import com.vinay.authify.dto.ProfileResponse;
 public interface ProfileService {
     ProfileResponse createProfile(ProfileRequest request);
     ProfileResponse getProfile(String email);
+    void sendResetOtp(String email);
 }
