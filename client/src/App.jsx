697a3f215b14347bfcba2f3376fd33544f7bcae4
code-walker-23 <chhabravinay549@gmail.com>
2025-06-07 12:24:33 +0530

    Added Routing Configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
+
 function App() {
   return (
-    <div>Hello</div>
-  )
-};
+    <div>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
+}
 
-export default App
+export default App;
